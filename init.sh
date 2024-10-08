cddbbba8e60b618efb46d65dc2f5672908a85a0d
chenxuan <77540051+chenxuan520@users.noreply.github.com>
2024-10-08 13:02:06 +0000

    feat:change download url

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # obsidian-export
-wget https://github.com/chenxuan520/obsidian2web/releases/download/v0.0.1/obsidian-export
+wget https://github.com/chenxuan520/obsidian-export/releases/latest/download/obsidian-export
 chmod +x ./obsidian-export
 # mdbook
 wget https://github.com/chenxuan520/obsidian2web/releases/download/v0.0.1/mdbook
