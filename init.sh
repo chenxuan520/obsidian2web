117ad7bc9dfc4fcd9194541b7f36e0caf961b95f
lingchen.judy <lingchen.judy@bytedance.com>
2024-10-10 13:13:30 +0800

    fix:update bin url

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # obsidian-export
-wget https://github.com/chenxuan520/obsidian-export/releases/latest/download/obsidian-export
+wget https://github.com/chenxuan520/obsidian-export/releases/latest/download/obsidian-export-ubuntu-latest -O obsidian-export
 chmod +x ./obsidian-export
 # mdbook
 wget https://github.com/chenxuan520/obsidian2web/releases/download/v0.0.1/mdbook
